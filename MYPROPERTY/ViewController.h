b8021ade8060408204d5f04668f9a2a66195655c
Somenath <IOS2@IOS2s-iMac.local>
2015-07-03 12:56:39 +0530

    Initial Commit

---
+++ MYPROPERTY/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MYPROPERTY
+//
+//  Created by Somenath on 03/07/15.
+//  Copyright (c) 2015 Esolz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
